e9e0dd2556c6a8102e828143bd58b1eafb3a839c
Mark Kirby <markkirby1997@gmail.com>
2025-07-24 00:57:49 -0400

    finished navigation bar to include all the sub tree's of the navigation JSON. This will now print all of the necessary items.

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -62,6 +62,7 @@ export default function AppSidebar() {
                          <SidebarMenu>
                             {filterNavigation.map((item) => (
                                 item.children && item.children.length > 0 ? (
+                                    /* If the item has children, use Collapsible for a dropdown effect */
                                     <Collapsible className="group/collapsible">
                                         <CollapsibleTrigger asChild>
                                             <SidebarMenuButton>
@@ -72,26 +73,48 @@ export default function AppSidebar() {
                                         <CollapsibleContent>
                                             <SidebarMenu>
                                                 {
+                                                /* Filter children based on permissions */
                                                 item.children.filter((x) => x.permissions.length === 0 || x.permissions.some(role => sessionData.userData[0].permissions.includes(role))).map((subItem,) => (
+                                                    /* If the subItem has children, create a sub-menu */
                                                     <SidebarMenuSub key={subItem.label}>
                                                         <SidebarMenuSubItem>
+                                                            {subItem.children && subItem.children.length > 0 ? (
+                                                            <Collapsible className="group/collapsible">
+                                                                <CollapsibleTrigger asChild>
+                                                                    <SidebarMenuButton>
+                                                                        {subItem.icon && iconMap[subItem.icon as keyof typeof iconMap]}
+                                                                        <span>{subItem.label}</span>
+                                                                        <ChevronUp className="ml-auto" />
+                                                                    </SidebarMenuButton>
+                                                                </CollapsibleTrigger>
+                                                                <CollapsibleContent>
+                                                                    <SidebarMenu>
+                                                                        {/* Filter sub-sub-items based on permissions */
+                                                                        subItem.children.filter((y) => y.permissions.length === 0 || y.permissions.some(role =>sessionData.userData[0].permissions.includes(role))).map((subSubItem) => (
+                                                                            <SidebarMenuItem key={subSubItem.label}>
+                                                                                <SidebarMenuButton asChild>
+                                                                                    <Link to={subSubItem.path}>
+                                                                                        {subSubItem.icon && iconMap[subSubItem.icon as keyof typeof iconMap]}
+                                                                                        <span>{subSubItem.label}</span>
+                                                                                    </Link>
+                                                                                </SidebarMenuButton>
+                                                                            </SidebarMenuItem>
+                                                                        ))}
+                                                                    </SidebarMenu>
+                                                                </CollapsibleContent>
+                                                            </Collapsible>
+                                                                   
+                                                            ) :(
                                                             <SidebarMenuButton asChild>
                                                                 <Link to={subItem.path}>
                                                                     {subItem.icon && iconMap[subItem.icon as keyof typeof iconMap]}
                                                                     <span>{subItem.label}</span>
                                                                 </Link>
                                                             </SidebarMenuButton>
+                                                            )}
                                                         </SidebarMenuSubItem>
                                                     </SidebarMenuSub>
                                                 ))}
-                                                <SidebarMenuItem>
-                                                    <SidebarMenuButton asChild >
-                                                        <Link to="/" key={item.label}>
-                                                            {item.icon && iconMap[item.icon as keyof typeof iconMap]}
-                                                            <span>{item.label}</span>
-                                                        </Link>
-                                                    </SidebarMenuButton>
-                                                </SidebarMenuItem>
                                             </SidebarMenu>
                                         </CollapsibleContent>
                                     </Collapsible>
